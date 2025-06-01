a5386455a23a098c4b84967eccce27030c9101f3
popdxo <sharkmouth20201@gmail.com>
2025-06-01 00:53:11 -0400

    addded stuifgf

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,36 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use mouse_keyboard_input::*;
+use std::time::Duration;
+use std::thread::{self};
+use std::sync::atomic::*;
+
+static CLICKING: AtomicBool = AtomicBool::new(false);
+static INTERVAL: AtomicU32 = AtomicU32::new(1000);
+
+fn auto_click() {
+    thread::spawn(|| {
+        let mut mouse = VirtualDevice::default().unwrap();
+        loop {
+            if !CLICKING.load(Ordering::Relaxed) {
+                continue;
+            }
+            thread::sleep(Duration::from_micros(INTERVAL.load(Ordering::Relaxed) as u64 ));
+            mouse.click(BTN_LEFT).unwrap();
+        }
+    });
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn toggle_clicker(interval: u32) {
+    INTERVAL.store(interval, Ordering::Relaxed);
+    CLICKING.store(!CLICKING.load(Ordering::Relaxed), Ordering::Relaxed);
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![toggle_clicker])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,31 +6,8 @@ use std::thread::{self, JoinHandle};
 use std::time::Duration;
 use std::sync::{Arc, Mutex};
 use std::sync::atomic::{AtomicBool, Ordering};
-
-
-static interval: u64 = 0;
-
-static CLICKER_THREAD: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
+use mouse_keyboard_input::*;
 
 fn main() {
     first_lib::run()
-
-    
-}
-
-// Starts Clicker Thread
-// Clicks until Clicker Thread is stopped
-#[tauri::command]
-fn begin_clicker(ms_interval: i32, button: String, position: Vec<i32>) {
-    static VALUE: bool = CLICKER_THREAD.load(Ordering::Relaxed);
-    let CLICKER_THREAD_CLONE: Arc<AtomicBool> = Arc::clone(&CLICKER_THREAD);
-
-    handler.join().unwrap();
-
-    
-}
-// Ends Clicker Thread
-#[tauri::command]
-fn end_clicker() {
-    
 }
